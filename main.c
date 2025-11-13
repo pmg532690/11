39a9fd346c93811294fce7beac4c6eccc0b27eed
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-11-13 15:51:26 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 char *proverb= "All that glisters is not gold.";
-void setPointer(char *q)
+void setPointer(char **q)
 {
-	q = proverb;
+	*q = proverb;
 }
 int main(void){
+	
 	char *p = "zzz";
-	setPointer(p);
+	setPointer(&p);
 	printf("%s\n", p);
 	
 	return 0;
